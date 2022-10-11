d24445f74b6ce6269c91fa6e4ee8cf7949b22bcf
ZY <zzymoon@gmail.com>
2022-10-11 10:00:12 +0800

    Initial Commit

---
+++ TestMenuInSafeArea/TestMenuInSafeAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMenuInSafeAreaApp.swift
+//  TestMenuInSafeArea
+//
+//  Created by Dio on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct TestMenuInSafeAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
